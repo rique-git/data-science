7e0bfe3bc036d2461628f35d8b8405b81f0d5958
rique <henriquemdanjos@gmail.com>
2023-12-20 23:48:52 +0000

    export data to csv

--- riotProject/collect_data.py
+++ riotProject/collect_data.py
@@ -1,6 +1,11 @@
 import argparse
 import requests
 from datetime import datetime
+import pandas as pd 
+
+
+data = pd.read_csv("games_data.csv", index_col="datetime")
+data = data.head(0)
 
 parser = argparse.ArgumentParser()
 
@@ -41,7 +46,7 @@ print("\nRequest to MatchV5    :", resp, "\n")
 matches_ids = resp.json()
 
 games_info = []
-for match_id in matches_ids[:20]:
+for match_id in matches_ids[:-1]:
     request_match_link = "https://europe.api.riotgames.com/lol/match/v5/matches/" + match_id + "?api_key=" + riotAPIkey
     resp = requests.get(request_match_link)
     print("Request to MatchV5 matches :", resp)
@@ -54,12 +59,48 @@ for i in games_info:
     date = datetime.fromtimestamp(int(str(game_info["info"]["gameCreation"])[:10]))
     game_type = game_info["info"]["gameMode"]
 
-#    print("------Blue Team-------")
-#    for player in game_info["info"]["participants"]:
-#        if player["teamId"] == 100:
-#            print("player =", player["summonerName"])
-#            print("champion =", player["championName"])
-#            print("position =", player["teamPosition"], "\n")
+    blue_team = []
+    red_team = []
+
+    for player in game_info["info"]["participants"]:
+        if player["summonerName"] == username:
+            if player["teamId"] == 100:
+                team = "blue"
+            else:
+                team = "red"
+            champion = player["championName"]
+            position = player["teamPosition"]
+            if position == "UTILITY":
+                position = "SUPPORT"
+            win = player["win"]
+
+        elif player["teamId"] == 100:
+            blue_team.append(player["championName"])
+        else:
+            red_team.append(player["championName"])
+    
+
+
+    # Create CSV with data
+    #datetime,game type,champion played,team,position,win,alied1,alied2,alied3,alied4,enemy1,enemy2,enemy3,enemy4,enemy5
+    alied_team = []
+    enemy_team = []
+
+    if len(blue_team) == 4: 
+        alied_team = blue_team
+        enemy_team = red_team
+    else:
+        alied_team = red_team
+        enemy_team = blue_team
+            
+    new_row_data = {'game type': game_type, 'champion played': champion, 'team': team, 'position': position\
+                    , 'win': win, 'alied1': alied_team[0], 'alied2': alied_team[1], 'alied3': alied_team[2], 'alied4': alied_team[3]\
+                    , 'enemy1': enemy_team[0], 'enemy2': enemy_team[1], 'enemy3': enemy_team[2], 'enemy4': enemy_team[3], 'enemy5': enemy_team[4]}
+    data.loc[date] = new_row_data
+
+data.to_csv("games_data.csv", index=True)
+
+
 
 
 
